b75cfb361ad6405e2de325718b8d28df24cd5c95
Adityesh <adityeshmishra98@gmail.com>
2020-07-08 22:58:35 +0530

    Endpoints for movies, software, games, ebooks, television added

--- api/routes.js
+++ api/routes.js
@@ -5,6 +5,21 @@ const controller = require('../controller/controller')
 
 router.get('/', controller.getTop50);
 
+router.get('/movies', controller.getMovies);
+
+router.get('/television', controller.getTelevision)
+
+router.get('/games', controller.getGames);
+
+router.get('/music', controller.getMusic);
+
+router.get('/books', controller.getEbooks);
+
+router.get('/software', controller.getSoftware);
+
+
+
+
 
 
 module.exports = router;
--- controller/controller.js
+++ controller/controller.js
@@ -36,26 +36,190 @@ module.exports =  {
     },
 
     getMovies : (req, res) => {
+        const uri = `https://www.torrentfunk.com/movies.html`
+        request.get(uri, (error, response, body) => {
+            let results = [];
 
+            if(error) res.status(500).json({"error" : "Internal server error."})
+            if(body) {
+
+                const $ = cheerio.load(body);
+                
+                $('table.tmain > tbody > tr').each((i, item) => {
+                    const torrent = {};
+                    if(i !== 0) {
+                        let id = $(item).first().find('a').attr('href').match(/(?<=torrent\/)(\d)*/g)[0]
+                        torrent.name = $(item).first().find('a').text();
+                        torrent.date = $(item).find('.tb').text();
+                        torrent.seeds = $(item).find('.tul').text();
+                        torrent.peers = $(item).find('.tdl').text();
+                        torrent.size = $(item).find('.tc').first().text()
+                        torrent.url = `https://www.torrentfunk.com/tor/${id}.torrent` 
+                    }
+                    
+                    
+                    results.push(torrent)
+                })
+            }
+            results.shift()
+            res.json(results)
+        })
     },
     
     getGames : (req, res) => {
+        const uri = `https://www.torrentfunk.com/games.html`
+        request.get(uri, (error, response, body) => {
+            let results = [];
 
+            if(error) res.status(500).json({"error" : "Internal server error."})
+            if(body) {
+
+                const $ = cheerio.load(body);
+                
+                $('table.tmain > tbody > tr').each((i, item) => {
+                    const torrent = {};
+                    if(i !== 0) {
+                        let id = $(item).first().find('a').attr('href').match(/(?<=torrent\/)(\d)*/g)[0]
+                        torrent.name = $(item).first().find('a').text();
+                        torrent.date = $(item).find('.tb').text();
+                        torrent.seeds = $(item).find('.tul').text();
+                        torrent.peers = $(item).find('.tdl').text();
+                        torrent.size = $(item).find('.tc').first().text()
+                        torrent.url = `https://www.torrentfunk.com/tor/${id}.torrent` 
+                    }
+                    
+                    
+                    results.push(torrent)
+                })
+            }
+            results.shift()
+            res.json(results)
+        })
     },
 
     getMusic : (req, res) => {
+        const uri = `https://www.torrentfunk.com/music.html`
+        request.get(uri, (error, response, body) => {
+            let results = [];
 
+            if(error) res.status(500).json({"error" : "Internal server error."})
+            if(body) {
+
+                const $ = cheerio.load(body);
+                
+                $('table.tmain > tbody > tr').each((i, item) => {
+                    const torrent = {};
+                    if(i !== 0) {
+                        let id = $(item).first().find('a').attr('href').match(/(?<=torrent\/)(\d)*/g)[0]
+                        torrent.name = $(item).first().find('a').text();
+                        torrent.date = $(item).find('.tb').text();
+                        torrent.seeds = $(item).find('.tul').text();
+                        torrent.peers = $(item).find('.tdl').text();
+                        torrent.size = $(item).find('.tc').first().text()
+                        torrent.url = `https://www.torrentfunk.com/tor/${id}.torrent` 
+                    }
+                    
+                    
+                    results.push(torrent)
+                })
+            }
+            results.shift()
+            res.json(results)
+        })
     },
 
     getEbooks : (req, res) => {
 
+        const uri = `https://www.torrentfunk.com/ebooks.html`
+        request.get(uri, (error, response, body) => {
+            let results = [];
+
+            if(error) res.status(500).json({"error" : "Internal server error."})
+            if(body) {
+
+                const $ = cheerio.load(body);
+                
+                $('table.tmain > tbody > tr').each((i, item) => {
+                    const torrent = {};
+                    if(i !== 0) {
+                        let id = $(item).first().find('a').attr('href').match(/(?<=torrent\/)(\d)*/g)[0]
+                        torrent.name = $(item).first().find('a').text();
+                        torrent.date = $(item).find('.tb').text();
+                        torrent.seeds = $(item).find('.tul').text();
+                        torrent.peers = $(item).find('.tdl').text();
+                        torrent.size = $(item).find('.tc').first().text()
+                        torrent.url = `https://www.torrentfunk.com/tor/${id}.torrent` 
+                    }
+                    
+                    
+                    results.push(torrent)
+                })
+            }
+            results.shift()
+            res.json(results)
+        })
+
     },
 
     getTelevision : (req, res) => {
+        const uri = `https://www.torrentfunk.com/television.html`
+        request.get(uri, (error, response, body) => {
+            let results = [];
 
+            if(error) res.status(500).json({"error" : "Internal server error."})
+            if(body) {
+
+                const $ = cheerio.load(body);
+                
+                $('table.tmain > tbody > tr').each((i, item) => {
+                    const torrent = {};
+                    if(i !== 0) {
+                        let id = $(item).first().find('a').attr('href').match(/(?<=torrent\/)(\d)*/g)[0]
+                        torrent.name = $(item).first().find('a').text();
+                        torrent.date = $(item).find('.tb').text();
+                        torrent.seeds = $(item).find('.tul').text();
+                        torrent.peers = $(item).find('.tdl').text();
+                        torrent.size = $(item).find('.tc').first().text()
+                        torrent.url = `https://www.torrentfunk.com/tor/${id}.torrent` 
+                    }
+                    
+                    
+                    results.push(torrent)
+                })
+            }
+            results.shift()
+            res.json(results)
+        })
     },
 
-    getSoftware : () => {
+    getSoftware : (req, res) => {
+        const uri = `https://www.torrentfunk.com/software.html`
+        request.get(uri, (error, response, body) => {
+            let results = [];
 
+            if(error) res.status(500).json({"error" : "Internal server error."})
+            if(body) {
+
+                const $ = cheerio.load(body);
+                
+                $('table.tmain > tbody > tr').each((i, item) => {
+                    const torrent = {};
+                    if(i !== 0) {
+                        let id = $(item).first().find('a').attr('href').match(/(?<=torrent\/)(\d)*/g)[0]
+                        torrent.name = $(item).first().find('a').text();
+                        torrent.date = $(item).find('.tb').text();
+                        torrent.seeds = $(item).find('.tul').text();
+                        torrent.peers = $(item).find('.tdl').text();
+                        torrent.size = $(item).find('.tc').first().text()
+                        torrent.url = `https://www.torrentfunk.com/tor/${id}.torrent` 
+                    }
+                    
+                    
+                    results.push(torrent)
+                })
+            }
+            results.shift()
+            res.json(results)
+        })
     }
 }
